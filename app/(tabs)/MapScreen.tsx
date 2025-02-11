69e7f4507bc142fde90cddec6c5726a7f98a7ca7
TakeshiAono <takeshi.aono001@gmail.com>
2025-02-11 18:27:19 +0900

    地図を実装

---
+++ app/(tabs)/MapScreen.tsx
@@ -0,0 +1,73 @@
+import { StyleSheet, Image, Platform, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useState, useEffect, useMemo } from 'react';
+
+import * as Location from 'expo-location';
+import MapView, { Region } from 'react-native-maps';
+
+export default function MapScreen() {
+  const [region, setRegion] = useState<Region>({
+    latitude: 36.2048,
+    longitude: 138.2529,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02,
+  })
+  const [isFirstUpdate, setIsFirstUpdate] = useState(true); // ✅ 初回更新フラグ
+
+  useEffect(() => {
+    async function confirmPermission() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+    }
+
+    confirmPermission();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsMyLocationButton={true}
+        region={region}
+        onUserLocationChange={(event) => {
+          // NOTE: 初期マップ表示の時にだけ発火し、現在位置の表示範囲に書き換える
+          if (!isFirstUpdate || !event.nativeEvent.coordinate) return;
+
+          event.persist()
+          setRegion({
+              latitude: event.nativeEvent.coordinate?.latitude ?? 36.2048,
+              longitude: event.nativeEvent.coordinate?.longitude ?? 138.2529,
+              latitudeDelta: region.latitudeDelta,
+              longitudeDelta: region.longitudeDelta,
+            }
+          );
+          setIsFirstUpdate(false)
+        }}
+        onRegionChange={(_, details) => {
+          if (details.isGesture) {
+            setRegion((prev) => {
+              return {
+                latitude: prev.latitude,
+                longitude: prev.longitude,
+                latitudeDelta: prev.latitudeDelta,
+                longitudeDelta: prev.longitudeDelta,
+              }
+            })
+          }
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,17 +27,17 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
+        name="MapScreen"
+        options={{
+          title: 'MapScreen',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="map.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
