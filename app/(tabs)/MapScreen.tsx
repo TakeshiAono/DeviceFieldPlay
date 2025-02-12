96a0e8a6310582b1e4561cc1ed667771ca872262
TakeshiAono <takeshi.aono001@gmail.com>
2025-02-12 23:17:04 +0900

    フォーマット適用

--- app/(tabs)/MapScreen.tsx
+++ app/(tabs)/MapScreen.tsx
@@ -1,28 +1,35 @@
-import { StyleSheet, Image, Platform, Text, View, Button } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Image, Platform, Text, View, Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
-import * as Location from 'expo-location';
-import MapView, { LatLng, MapPolygon, Marker, Polygon, Polyline, Region } from 'react-native-maps';
+import * as Location from "expo-location";
+import MapView, {
+  LatLng,
+  MapPolygon,
+  Marker,
+  Polygon,
+  Polyline,
+  Region,
+} from "react-native-maps";
 
 const initialJapanRegion = {
   latitude: 36.2048,
   longitude: 138.2529,
   latitudeDelta: 0.001,
   longitudeDelta: 0.001,
-}
+};
 
 export default function MapScreen() {
-  const [region, setRegion] = useState<Region>(initialJapanRegion)
-  const [markers, setMarkers] = useState<(LatLng & {key: number})[]>([])
-  const i = useRef(1)
+  const [region, setRegion] = useState<Region>(initialJapanRegion);
+  const [markers, setMarkers] = useState<(LatLng & { key: number })[]>([]);
+  const i = useRef(1);
   const [isFirstUpdate, setIsFirstUpdate] = useState(true); // ✅ 初回更新フラグ
 
   useEffect(() => {
     async function confirmPermission() {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
+      if (status !== "granted") {
         return;
       }
     }
@@ -31,13 +38,13 @@ export default function MapScreen() {
   }, []);
 
   const resetMarkers = () => {
-    i.current = 1
-    setMarkers([])
-  }
+    i.current = 1;
+    setMarkers([]);
+  };
 
   return (
     <SafeAreaView>
-      <Button title={"リセット"}  onPress={resetMarkers}/>
+      <Button title={"リセット"} onPress={resetMarkers} />
       <MapView
         style={styles.map}
         showsUserLocation={true}
@@ -45,22 +52,28 @@ export default function MapScreen() {
         showsMyLocationButton={true}
         region={region}
         onLongPress={(event) => {
-          setMarkers([...markers, {...event.nativeEvent.coordinate, key: i.current}])
-          i.current += 1
+          setMarkers([
+            ...markers,
+            { ...event.nativeEvent.coordinate, key: i.current },
+          ]);
+          i.current += 1;
         }}
         onUserLocationChange={(event) => {
           // NOTE: 初期マップ表示の時にだけ発火し、現在位置の表示範囲に書き換える
           if (!isFirstUpdate || !event.nativeEvent.coordinate) return;
 
-          event.persist()
+          event.persist();
           setRegion({
-              latitude: event.nativeEvent.coordinate?.latitude ?? initialJapanRegion.latitude,
-              longitude: event.nativeEvent.coordinate?.longitude ?? initialJapanRegion.longitude,
-              latitudeDelta: region.latitudeDelta,
-              longitudeDelta: region.longitudeDelta,
-            }
-          );
-          setIsFirstUpdate(false)
+            latitude:
+              event.nativeEvent.coordinate?.latitude ??
+              initialJapanRegion.latitude,
+            longitude:
+              event.nativeEvent.coordinate?.longitude ??
+              initialJapanRegion.longitude,
+            latitudeDelta: region.latitudeDelta,
+            longitudeDelta: region.longitudeDelta,
+          });
+          setIsFirstUpdate(false);
         }}
         onRegionChange={(_, details) => {
           if (details.isGesture) {
@@ -70,17 +83,21 @@ export default function MapScreen() {
                 longitude: prev.longitude,
                 latitudeDelta: prev.latitudeDelta,
                 longitudeDelta: prev.longitudeDelta,
-              }
-            })
+              };
+            });
           }
         }}
       >
-        {markers.map(marker => <Marker
-          key={marker.key}
-          coordinate={{ latitude: marker.latitude, longitude: marker.longitude }} // 東京
-          title={marker.key.toString()}
-          />)
-        }
+        {markers.map((marker) => (
+          <Marker
+            key={marker.key}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }} // 東京
+            title={marker.key.toString()}
+          />
+        ))}
         <Polyline
           coordinates={markers}
           strokeWidth={5} // 線の太さ
@@ -93,7 +110,7 @@ export default function MapScreen() {
 
 const styles = StyleSheet.create({
   map: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
