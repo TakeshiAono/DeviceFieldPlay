438c23f0f8358020f40f146cc911898ec7b864f4
TakeshiAono <takeshi.aono001@gmail.com>
2025-05-05 16:46:42 +0900

    eventBridgeスケジューラーを登録するlambdaを実装

---
+++ lambda/gameEndScheduleSetter/index.mjs
@@ -0,0 +1,88 @@
+import { readFileSync } from "fs";
+import googleAuthLibrary from "google-auth-library";
+import {
+  SchedulerClient,
+  CreateScheduleCommand,
+} from "@aws-sdk/client-scheduler";
+
+const AWS_REGION = process.env.REGION;
+
+const { JWT } = googleAuthLibrary;
+
+const scheduleClient = new SchedulerClient({ region: AWS_REGION });
+
+// Lambda ハンドラー
+export const handler = async (event) => {
+  if (
+    event.Records[0].dynamodb.OldImage == undefined ||
+    event.Records[0].dynamodb.NewImage == undefined ||
+    !event.Records[0].dynamodb.NewImage.isGameStarted.BOOL
+  )
+    return;
+
+  try {
+    const gameTimeLimit = event.Records[0].dynamodb.NewImage.gameTimeLimit.S;
+    console.log(gameTimeLimit);
+    if (gameTimeLimit === "") return;
+    const date = new Date(gameTimeLimit);
+    const formattedGameTimeLimit = date.toISOString().split(".")[0];
+
+    await createSchedule({
+      scheduleName:
+        event.Records[0].dynamodb.NewImage.id.S +
+        +date.getFullYear() +
+        date.getDate() +
+        date.getHours() +
+        date.getMinutes() +
+        date.getSeconds(),
+      targetLambdaArn: process.env.TARGET_LAMBDA_ARN,
+      roleArn: process.env.ROLE_ARN,
+      scheduleTimeUtc: formattedGameTimeLimit, // JST 15:30 相当
+      gameId: event.Records[0].dynamodb.NewImage.id.S,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true }),
+    };
+  } catch (error) {
+    console.error(
+      "FCM API Error:",
+      error.response ? error.response.data : error.message,
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ error: "Failed to send FCM message" }),
+    };
+  }
+};
+
+export async function createSchedule({
+  scheduleName, // 例: "schedule-dynamo-abc123"
+  targetLambdaArn, // Lambda 関数の ARN
+  roleArn, // スケジューラーが Lambda を呼び出すための IAM ロール
+  scheduleTimeUtc, // 例: "2025-05-06T06:30:00Z"
+  dynamoId, // 任意の ID を渡したい
+}) {
+  const command = new CreateScheduleCommand({
+    Name: scheduleName,
+    ScheduleExpression: `at(${scheduleTimeUtc})`,
+    GroupName: "default", // ← 明示的に追加！
+    FlexibleTimeWindow: { Mode: "OFF" }, // 固定時刻実行
+    Target: {
+      Arn: targetLambdaArn,
+      RoleArn: roleArn,
+      Input: JSON.stringify({
+        dynamoId,
+      }),
+    },
+  });
+
+  try {
+    await scheduleClient.send(command);
+    console.log(`✅ スケジュール '${scheduleName}' を作成しました`);
+  } catch (error) {
+    console.error("❌ スケジュール作成失敗:", error);
+    throw error;
+  }
+}
