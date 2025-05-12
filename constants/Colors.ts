82913c29be9121412b919a9005a625c73e96408e
TakeshiAono <takeshi.aono001@gmail.com>
2025-05-13 02:03:27 +0900

    💄 カラーテーマの定義

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -10,22 +10,15 @@ const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
 
 export const Colors = {
-  light: {
-    text: "#11181C",
-    background: "#fff",
-    tint: tintColorLight,
-    icon: "#687076",
-    tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: "#ECEDEE",
-    background: "#151718",
-    tint: tintColorDark,
-    icon: "#9BA1A6",
-    tabIconDefault: "#9BA1A6",
-    tabIconSelected: tintColorDark,
-  },
+  primary: "#21D7E0",
+  secondary: "#E06D22",
+
+  success: "#54E189",
+  warning: "#E0DC61",
+  info: "#3F52E0",
+  error: "#E05A53",
+
+  inactive: "#DEE2E0",
 };
 
 export const MapAreaColors = {
