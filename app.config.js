5b7be0b23151a7c6c1ab46eca8601465364a3edf
TakeshiAono <takeshi.aono001@gmail.com>
2025-06-01 01:09:52 +0900

    ♻️ フォーマット適用

--- app.config.js
+++ app.config.js
@@ -1,79 +1,80 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-  "expo": {
-    "name": "DeviceFieldPlay",
-    "slug": "DeviceFieldPlay",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "extra": {
-      "awsAccessKeyId": process.env.AWS_ACCESS_KEY_ID,
-      "awsSecretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
-      "awsRegion": process.env.AWS_REGION,
-      "eas": {
-        "projectId": process.env.PROJECT_ID
-      }
+  expo: {
+    name: "DeviceFieldPlay",
+    slug: "DeviceFieldPlay",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    extra: {
+      awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      awsRegion: process.env.AWS_REGION,
+      eas: {
+        projectId: process.env.PROJECT_ID,
+      },
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.aonot.devicefield"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.aonot.devicefield",
     },
-    "android": {
-      "config": {
-        "googleMaps": {
-          "apiKey": process.env.GOOGLE_MAPS_API_KEY,
+    android: {
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
         },
       },
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "package": "com.aonot.devicefield",
-      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
+      package: "com.aonot.devicefield",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
-    "plugins": [
+    plugins: [
       "expo-router",
       [
         "expo-splash-screen",
         {
-          "image": "./assets/images/icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff"
-        }
+          image: "./assets/images/icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
       ],
       [
         "expo-camera",
         {
-          "cameraPermission": "カメラの権限を許可してください"
-        }
+          cameraPermission: "カメラの権限を許可してください",
+        },
       ],
       [
         "expo-location",
         {
-          "locationAlwaysAndWhenInUsePermission": "位置情報の取得を許可してください",
-        }
+          locationAlwaysAndWhenInUsePermission:
+            "位置情報の取得を許可してください",
+        },
       ],
       [
         "expo-notifications",
         {
-          "color": "#ffffff",
-          "defaultChannel": "default",
-          "enableBackgroundRemoteNotifications": true
-        }
+          color: "#ffffff",
+          defaultChannel: "default",
+          enableBackgroundRemoteNotifications: true,
+        },
       ],
     ],
-    "experiments": {
-      "typedRoutes": true
-    }
-  }
-}
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
