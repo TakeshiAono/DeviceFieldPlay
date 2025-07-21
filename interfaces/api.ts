35a6908ade9dcc85ba52e1b3bf4cc52402cde04a
TakeshiAono <takeshi.aono001@gmail.com>
2025-07-21 19:01:24 +0900

    🏷️ 必要なインターフェースの定義

--- interfaces/api.ts
+++ interfaces/api.ts
@@ -1,5 +1,8 @@
-import { PutCommandInput, PutCommandOutput } from "@aws-sdk/lib-dynamodb";
+import { LatLng } from "react-native-maps";
+import { PutCommandOutput } from "@aws-sdk/lib-dynamodb";
+
 import { AbilityList } from "./abilities";
+import { Props as UserProps } from "@/models/UserModel";
 
 type GameId = string;
 
@@ -40,3 +43,50 @@ export type DynamoDevice = {
 export type PutDynamoTagGame = (
   item: DynamoTagGame,
 ) => Promise<PutCommandOutput>;
+
+/**
+ * publisherがレーダーアビリティを実行した際にAPIリクエストをするインターフェース
+ *
+ * @param gameId: lambdaにてsubユーザーのpush通知トークンをDynamoのusersテーブルから検索するために使用する。
+ * @param currentPosition: push通知先のデバイスにてpublisherの位置情報、subscriber側で距離を算出し自分がレーダー範囲内(50m以内)に該当するか計算する際に使用する。
+ * @param publishId: push通知先のデバイスを経てDynamoのRadarlocationsテーブルに保存する際に使用する、後からpubがポーリングした際の検索キーとして使用する。
+ */
+export interface ExecDistanceForRadarRequest {
+  (
+    gameId: DynamoTagGame["id"],
+    currentPosition: LatLng,
+    publishId: UserProps["id"],
+  ): Promise<void>;
+}
+
+/**
+ * レーダー範囲内に該当するsubscriberがRaderLocationsテーブルに情報をAPIリクエストにて保存するインターフェース
+ */
+export interface PostRadarLocation {
+  (args: DynamoRadarLocation): Promise<void>;
+}
+
+/**
+ * RaderLocationsテーブルの定義
+ *
+ * @param id: プライマリキー
+ * @param publishId: pubがポーリングの時に使用し、自分のuserIdとpublishIdのレコードを取得する際に使用する検索キー
+ * @param location: 現在は使い道はないが、今後詳細な位置を割り出したいなどのアビリティのために保存しておく
+ * @param expiresAt: 同じアビリティが実行された場合に古いアビリティの結果が新しいアビリティと混ざらないようにするための値、有効期限が古いものは取得しないようにするための値
+ */
+export type DynamoRadarLocation = {
+  id: number;
+  publishId: UserProps["id"];
+  location: LatLng;
+  expiresAt: Date;
+};
+
+/**
+ * ExecDistanceForRadarRequestが発火して10秒後にポーリング(1回)をする時のインターフェース
+ *
+ * @param publishId: 自分のユーザーIdにてDynamoのRadarLocationsテーブルから検索するために使用する。
+ * @returns レーダーに該当した人数分のlocation情報
+ */
+export interface GetLocationsByUserId {
+  (publishId: UserProps["id"]): Promise<DynamoRadarLocation[]>;
+}
